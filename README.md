317ddc03c7e0e51109b755006df2e14c0a6a7d0c
daro128 <p8735036@gmail.com>
2026-01-20 23:32:43 +0700

    done

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
 # Final.Project_java
+Java : Stock System 
+
+Intro  
+
+The system can check business stock and profit, and update stock. It can check current stock and profit by showing item’s ID, name, quantity, buy price, sell price, profit per item, and total profit for each item. 
+
+It also has an alert flag for items that are out of stock or almost out. Moreover, it has functionality that can update the stock such as adding item quantity, adding new items, and updating item quantity in stock. 
+
+Furthermore, the  system Supports two main user role: Admin and staff, and requires users to log in using a username and password to access the system. Admin has full control over stock and system management, while Staff is responsible for daily stock and sales operation . 
+
+The last function is monthly profit. It shows the data on profits and sales of items in each month. 
+
+Display  
+
+First 
+
+Admin Role( Requires password username to login to the next display) 
+
+Staff Role( Requires password username to login to the next display) 
+
+Second (Admin) 
+
+Check current stock & profit 
+
+Update stock 
+
+Add item 
+
+Add new item 
+
+Update item’s quantity 
+
+Monthly sales / profit (show the data of sale and profit of each month from the start of business) 
+
+Manage staff 
+
+Add staff 
+
+Delete staff  
+
+Update staff 
+
+Remove staff 
+
+   
+
+ 
+
+Second (Staff) 
+
+View product and stock information 
+
+Sell products  
+
+Full price 
+
+Discount price 
+
+Update stock  
+
+View low-stock alerts 
+
+Third (Show Data) 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+Variables  
+
+ID 
+
+Name 
+
+Quantity 
+
+Buy 
+
+Sell 
+
+Profit per item 
+
+Current total profit 
+
+Stock alert 
---
+++ main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int choice = 1;
+
+        do{
+            System.out.println("=============Welcome To Stock Management System==============");
+            System.out.println("1.Admin");
+            System.out.println("2.Staff");
+            System.out.println("3.Help");
+            System.out.println("0.Exit");
+            System.out.println("==============================================================");
+            System.out.print("Please Enter an option To continue (0-3): ");
+            choice = sc.nextInt();
+            
+            switch (choice) {
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
+                    
+                case 1:
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    System.out.println("=============Welcome To Stock Management System==============");
+                    System.out.print("Enter The Password To continue: ");
+                    int admin_password = sc.nextInt();
+                    if(admin_password == 12345){ 
+                        System.out.println("Login success...!"); 
+                        // Function Admin
+                    } else {
+                        System.out.println("Wrong password! Login Fail....!");
+                    }
+                    break;
+                    
+                case 2:
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    System.out.print("Enter User name and Id To continue: ");
+                    int staff_id = sc.nextInt();
+                    if(staff_id == 123){
+                        System.out.println("done..!"); 
+                        // Function staff
+                    }
+                    break;
+                    
+                case 3:
+                    System.out.println("Help.....!");
+                    break;
+            
+                default:
+                    System.out.println("\nInvalid Option! Please choose 0-3.");
+                    System.out.print("Press Enter to continue...");
+                    sc.nextLine();
+                    sc.nextLine();
+            }
+            
+        } while(choice != 0);
+        
+        sc.close();
+    }
+}
